2b7893ab0eb21bb781a1f5bb1d5ce61c2bff2175
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-04-04 18:38:59 -0400

    maquetando la pagina de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import '@styles/Checkout.css';
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>name</h4>
+            <span>$</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio total: $4</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
+  );
+};
 
-export  {Checkout}
+export { Checkout };
