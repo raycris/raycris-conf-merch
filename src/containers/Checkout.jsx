da692d98663fd53091e9934a71b670ecc5b6a314
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-04-05 16:10:47 -0400

    completando la pantalla de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import { AppContext } from '@contexts/AppContext';
 
 import '@styles/Checkout.css';
 
 const Checkout = () => {
+  const { state, removeFromCart } = useContext(AppContext);
+  const { cart } = state;
+
+  const handleRemove = (product) => () => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de Pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>name</h4>
-            <span>$</span>
+        {cart.length > 0 ? (
+          <h3>Lista de Pedidos:</h3>
+        ) : (
+          <h3>No hay pedidos disponibles.</h3>
+        )}
+        {cart.map((item) => (
+          <div className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+            <button type="button" onClick={handleRemove(item)}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
           </div>
-          <button type="button">
-          <i className='fas fa-trash-alt'></i>
-          </button>
+        ))}
+      </div>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio total: $ ${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
+            <button type="button">Continuar pedido</button>
+          </Link>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio total: $4</h3>
-        <Link to="/checkout/information">
-          <button type="button">Continuar pedido</button>
-        </Link>
-      </div>
+      )}
     </div>
   );
 };
